1e8254ac46cbb0594d750a957f8e7b1dd0a37f3e
mlepinski <mlepinski@gmail.com>
2021-08-23 15:21:31 -0400

    Add files via upload

---
+++ Assignment 1/Test1.java
@@ -0,0 +1,62 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
+import java.lang.RuntimeException;
+
+//IMPORTANT: Note that the processLetters function doesn't need to care at all about which implementation of a Letter is being used. 
+
+public class Test1{
+
+    //This function prints the letters in the input list
+    //... alternating between character and integer representation
+    static void processLetters( List<Letter> lst ){
+	System.out.println("");
+	System.out.println("-------------");
+	System.out.println("Printing Letters Now:");
+
+	boolean isChar = true; 
+	
+	for( Letter l : lst ){
+	    if( isChar ){
+		System.out.println(l.getChar());
+	    }
+	    else{
+		System.out.println(l.getInt());
+	    }
+	    isChar = ! isChar;
+	}
+    }
+
+    public static void main(String[] args){
+	List<Letter> my_list = new ArrayList<Letter>();
+
+	my_list.add( new CharLetter('A') );
+	my_list.add( new CharLetter('B') );
+	my_list.add( new IntLetter(89) );
+	my_list.add( new IntLetter(90) );
+
+	try{
+	    my_list.add( new CharLetter('%') );
+	}
+	catch(RuntimeException e){
+	    System.out.println("Correctly handled % error");
+	}
+
+	try{
+	    my_list.add( new IntLetter(93) );
+	}
+	catch(RuntimeException e){
+	    System.out.println("Correctly handled 93 error");
+	}
+
+	try{
+	    my_list.add( new CharLetter('m') );
+	}
+	catch(RuntimeException e){
+	    System.out.println("This Shouldn't be an Error, m is a letter");
+	}
+
+	processLetters(my_list);
+    }
+}
+	    
